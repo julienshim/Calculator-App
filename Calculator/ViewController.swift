0187cb5a43bb433161307947a86d996e0de5df32
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-02-11 19:50:01 -0800

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Julien Shim on 2/11/19.
+//  Copyright © 2019 Julien Shim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# Calculator App
+
+Simple calculator app built with Swift 4, Xcode 10.1, for iOS 12.1
+
+## Demo
+
+In development
