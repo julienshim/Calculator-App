94b3a1de1bc1de4cc1e62ac385a98d2fa04078ed
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-02-11 21:11:24 -0800

    ButtonUI linked

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,6 +16,50 @@ class ViewController: UIViewController {
     }
     
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        switch sender.tag {
+            case 1:
+                print("one")
+            case 2:
+                print("two")
+            case 3:
+                print("three")
+            case 4:
+                print("four")
+            case 5:
+                print("five")
+            case 6:
+                print("six")
+            case 7:
+                print("seven")
+            case 8:
+                print("eight")
+            case 9:
+                print("nine")
+            case 10:
+                print("zero")
+            case 11:
+                print("decimal")
+            case 12:
+                print("divide")
+            case 13:
+                print("multiply")
+            case 14:
+                print("subtract")
+            case 15:
+                print("add")
+            case 16:
+                print("equals")
+            case 17:
+                print("all clear")
+            case 18:
+                print("plus/negative")
+            case 19:
+                print("percent")
+            default: ()
+        }
+        
+    }
+    
 }
 
